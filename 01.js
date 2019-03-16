a9195f2ea056011597acbaf8cc42f763696693b9
agustin <carlos.agustin.diaz1998@gmail.com>
2019-03-16 14:05:03 -0300

    Clase n1

---
+++ 01.js
@@ -0,0 +1,12 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+      message: 'Hola mundo con vue',
+      frutas: ['Manzana','Pera','Naranja'],
+      fruts:[
+          {nombre:'Pera', cantidad:10},
+          {nombre:'Manzana', cantidad:0},
+          {nombre:'Naranja', cantidad:23}
+      ]
+    }
+  })
