dc3967464fa725550553959e9ff1a9584f294afb
agustin <carlos.agustin.diaz1998@gmail.com>
2019-03-19 20:14:07 -0300

    clase n13 actions + mutaciones, consumo de json de parte del back-end

--- componentes/hijo10.js
+++ componentes/hijo10.js
@@ -3,23 +3,38 @@ Vue.component('hijo',{
     <div>
     <button class="btn btn-primary" @click="aumentar">+</button>
     <button class="btn btn-primary" @click="disminuir(2)">-</button>
+    <button class="btn btn-primary" @click="obtenerCursos">Obtener cursos</button>
     <h1>Numero {{ numero}}</h1>
         <br />
         <input type="text" class="form-control" v-model="$store.state.message">
+
+        <hr>
+        <h3>Cursos</h3>
+        <ul >
+            <li v-for="item in cursos">
+                {{item.nombre}}
+            </li>
+        </ul>
     </div>      
     `,
     computed:{
         ...Vuex.mapState([
-            'numero'
+            'numero',
+            'cursos'
         ])
     },
     /*mapeo de mutaciones */
     methods:{
         ...Vuex.mapMutations([
             'aumentar',
-            'disminuir'
+            'disminuir',
+        ]),
+        //mapeo los actions
+        ...Vuex.mapActions([
+            'obtenerCursos'          
         ])
     }
+    
 });
 /*para llamar a una mutacion debo llamar primero un commit 
     y le paso como parametro el nombre de la funcion o mutacion */
--- js/10.js
+++ js/10.js
@@ -3,7 +3,8 @@ const store = new Vuex.Store({
     //creamos un estado
     state:{
         numero: 10,
-        message: ''
+        message: '',
+        cursos:[] /*se lelnara con el JSON */
     },/*creamos una mutacion que son funciones */
     mutations:{
         aumentar(state){
@@ -11,6 +12,19 @@ const store = new Vuex.Store({
         },
         disminuir(state,n){
             state.numero -= n
+        },
+        llenarCursos(state,cursosAccion){
+            state.cursos = cursosAccion
+        }
+    },
+    actions:{
+        /*funciones */
+        obtenerCursos: async function({ commit }){ /*el async debe ir siempre antes de function */
+            /*obtengo datos del JSON */
+            const dato = await fetch('js/cursos.json'); /*para poder trabajar con el await debe esta dentro del async */
+            /*me pare en el html !importante */
+            const cursos = await dato.json(); /* llena la variable cursos con lo que continene el JSON */
+            commit('llenarCursos',cursos); /*llamo a la mutacion  llenarCursos y le paso los cursos */
         }
     }
 });
